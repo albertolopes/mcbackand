ceb8695d01ce2c19b55b8eb38d356564218aa5bd
albertolopes <alberto_llopes@hotmail.com>
2020-03-30 21:38:47 -0300

    Criação validação personalizada para CPF E CNPJ

--- src/main/java/com/springboot/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/springboot/cursomc/dto/ClienteNewDTO.java
@@ -1,23 +1,47 @@
 package com.springboot.cursomc.dto;
 
+import com.springboot.cursomc.services.validation.ClienteInsert;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Size(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Email(message = "Email invalido")
     private String email;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String cpfOuCnpj;
+
     private Integer tipo;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String logradouro;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatorio")
     private String telefone1;
+
     private String telefone2;
+
     private String telefone3;
 
     private Integer cidadeId;
---
+++ src/main/java/com/springboot/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,41 @@
+package com.springboot.cursomc.services.validation;
+
+import com.springboot.cursomc.services.validation.utils.BR;
+import com.springboot.cursomc.domain.enums.TipoCliente;
+import com.springboot.cursomc.dto.ClienteNewDTO;
+import com.springboot.cursomc.repositories.ClienteRepository;
+import com.springboot.cursomc.resources.exception.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
