11a79013f979d079be46b3e27a6a0741d22745c1
albertolopes <alberto_llopes@hotmail.com>
2020-04-01 13:02:53 -0300

    Profile de desenvolvimento

--- src/main/java/com/springboot/cursomc/CursomcApplication.java
+++ src/main/java/com/springboot/cursomc/CursomcApplication.java
@@ -1,14 +1,5 @@
 package com.springboot.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import com.springboot.cursomc.domain.*;
-import com.springboot.cursomc.domain.enums.EstadoPagamento;
-import com.springboot.cursomc.domain.enums.TipoCliente;
-import com.springboot.cursomc.repositories.*;
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/springboot/cursomc/DevConfig.java
@@ -0,0 +1,31 @@
+package com.springboot.cursomc;
+
+import com.springboot.cursomc.services.DBsevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBsevice dBsevice;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+        dBsevice.instantiateTesteDatabase();
+        return true;
+    }
+
+}
